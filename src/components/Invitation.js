803c2c26cd891de78e16c0d5b8699ac47180d334
Ali Alsayed <a_alsayed87@hotmail.com>
2019-11-01 07:50:42 +0000

    Automatically backed up by Learn

--- src/components/Invitation.js
+++ src/components/Invitation.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react';
 
 class Invitation extends React.Component {
@@ -7,8 +8,21 @@ class Invitation extends React.Component {
       <div>
         <h1>You've been invited!</h1>
         {this.props.children}
+=======
+// Code Invitation Component Here
+import React from 'react';
+export default class Invitation extends React.Component{
+  render(){
+    return (
+      <div>
+        <h1>{"You've been invited!"}</h1>
+        <div>{this.props.children}</div>
+>>>>>>> 4b7db5bdd1d13b9f8c4b0edf88e058fa9c4cab4a
       </div>
     )
   }
 }
+<<<<<<< HEAD
 export default Invitation;
+=======
+>>>>>>> 4b7db5bdd1d13b9f8c4b0edf88e058fa9c4cab4a
